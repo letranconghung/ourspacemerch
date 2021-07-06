41faa16ef47e777b0c6e52f24093262b0658819e
Le Tran Cong Hung <conghungletran@gmail.com>
2021-07-06 13:23:24 +0800

    added sendOrder to realtime database

--- public/static/js/db.js
+++ public/static/js/db.js
@@ -1,16 +1,17 @@
 function upload(file, total, qty, inputName, inputClass){
   fileExt = file.name.split('.').pop();
+  var date = new Date();
+  var timestamp = date.getTime();
   console.log(fileExt);
-  uploadFileName = `${total}$_${qty[0]}stickers_${qty[1]}pins_${fullName}_${office}_${inputName}_${inputClass}.${fileExt}`;
+  uploadFileName = `${timestamp}_${total}$_${qty[0]}stickers_${qty[1]}pins_${fullName}_${office}_${inputName}_${inputClass}.${fileExt}`;
   console.log(uploadFileName);
   var storageRef = firebase.storage().ref(`records/${uploadFileName}`);
   var task = storageRef.put(file);
   task.on('state_changed', function progress(snapshot) {
-    var percentage = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
-    document.getElementById("uploader").value = percentage;
   }, function error(err) {
     alert("There was an error with the upload process, please try again.");
   },function complete() {
     console.log('upload completed');
+    sendOrder(date, qty, total, inputName, inputClass);
   });
 }
